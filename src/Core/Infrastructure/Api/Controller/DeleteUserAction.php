5b9cde7fbad7f2c22e590d59ca44e0e7692053cc
Alexey Musyka <alexmusyka11@gmail.com>
2024-03-04 17:24:56 +0300

    PT-7 Add Delete Action

---
+++ src/Core/Infrastructure/Api/Controller/DeleteUserAction.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Core\Infrastructure\Api\Controller;
+
+use App\Core\Infrastructure\Repository\UserRepository;
+use App\Shared\Infrastructure\Api\Controller\AbstractAction;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route(path: '/users/{id}', name: 'api.users.delete', methods: ['DELETE'])]
+class DeleteUserAction extends AbstractAction
+{
+    public function __invoke(
+        string $id,
+        Request $request,
+        UserRepository $userRepository,
+    ): JsonResponse {
+        $application = $this->getMyApplication($request);
+
+        $user = $userRepository->getActive($id, $application);
+
+        $user->block();
+
+        return new JsonResponse();
+    }
+}
